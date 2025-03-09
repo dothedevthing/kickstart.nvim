bc81b353893c3f54c336f3502ea741b10be96058
Chris Mizer <christopher.d.mizer.mil@army.mil>
2025-03-09 10:59:19 -0400

    Modularize setup

--- lua/custom/plugins/gitsigns.lua
+++ lua/custom/plugins/gitsigns.lua
@@ -3,7 +3,21 @@
 -- config. This will add also the recommended keymaps.
 
 return {
-  {
+
+	-- See `:help gitsigns` to understand what the configuration keys do
+	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+
+		--[[
     'lewis6991/gitsigns.nvim',
     opts = {
       on_attach = function(bufnr)
@@ -57,5 +71,8 @@ return {
         map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
       end,
     },
-  },
+  -- ]]
+	},
 }
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/lazy-plugins.lua
@@ -0,0 +1,43 @@
+require("lazy").setup({
+	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+
+	require 'custom/plugins/autopairs',
+	require("custom/plugins/cmp"),
+	require("custom/plugins/conform"),
+	require 'custom/plugins/debug',
+	require("custom/plugins/gitsigns"),
+	require("custom/plugins/indent_line"),
+	require("custom/plugins/lint"),
+	require("custom/plugins/lspconfig"),
+	require("custom/plugins/mini"),
+	require("custom/plugins/neo-tree"),
+	require("custom/plugins/telescope"),
+	require("custom/plugins/todo-comments"),
+	require("custom/plugins/tokyonight"),
+	require("custom/plugins/treesitter"),
+	require("custom/plugins/which-key"),
+
+	ui = {
+		-- If you are using a Nerd Font: set icons to an empty table which will use the
+		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
